07e82579ac9bfaac16308c3ddd065e07cfecf4a0
eduardoandd <eduardo.andrade.dev@gmail.com>
2025-01-25 18:46:54 -0300

    fazendo um get

---
+++ my-app/src/app/names/data.ts
@@ -0,0 +1,10 @@
+export const data = [
+    {
+        id:1,
+        name:'João'
+    },
+    {
+        id:2,
+        name:'Vitor'
+    },
+]
---
+++ my-app/src/app/names/route.ts
@@ -0,0 +1,6 @@
+import {data } from './data'
+
+export async function  GET() {
+    return Response.json(data)
+    
+}
