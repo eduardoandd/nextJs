5817e6effdc6bf0d74ab1027fe8b9123668c3cfe
eduardoandd <eduardo.andrade.dev@gmail.com>
2025-01-25 17:58:09 -0300

    template

--- my-app/src/app/(users)/template.tsx
+++ my-app/src/app/(users)/template.tsx
@@ -2,6 +2,7 @@
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 import './styles.css'
+import { useState } from "react"
 
 const navLinks = [
     {name:'Profile', href:'/profile'},
@@ -10,13 +11,15 @@ const navLinks = [
 ]
 
 export default function UsersLayout({children}: {children: React.ReactNode}){
+    const [value, setValue] = useState('')
     const pathName = usePathname()
     return(
         <div>
+            <div>
+                <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
+            </div>
             {navLinks.map((link) => {
-
                 const isActive = pathName.startsWith(link.href)
-
                 return(
                     <Link
                         className={`${isActive ? 'text-red-900' : 'text-blue-900'}`}
