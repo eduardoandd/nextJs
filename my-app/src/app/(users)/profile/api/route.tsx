e424889b5f5b545c27acb13a7a24fc01c2036167
eduardoandd <eduardo.andrade.dev@gmail.com>
2025-01-26 01:14:35 -0300

    entendendo cookies

--- my-app/src/app/(users)/profile/api/route.tsx
+++ my-app/src/app/(users)/profile/api/route.tsx
@@ -1,16 +1,24 @@
 import { NextRequest } from "next/server"
-import { headers } from "next/headers"
+import { headers, cookies } from "next/headers"
 
 export async function  GET(request: NextRequest) {
     const requestHeaders = new Headers(request.headers)
     const headerList = headers()
+    cookies().set('token','123abc')
+    const theme = request.cookies.get("theme")
 
 
     console.log(requestHeaders.get('Authorization'));
     console.log(headerList.get('Authorization'));
+    console.log(theme);
     
 
-    return new Response('<h1>Profile Route</h1>',{ headers:{"Content-Type":"text/html"}})
+    return new Response('<h1>Profile Route</h1>',
+        { headers:{
+            "Content-Type":"text/html",
+            "Set-Cookie": "theme=dark"
+        }}
+    )
 
     
 }
