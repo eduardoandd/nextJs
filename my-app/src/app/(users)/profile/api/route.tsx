cab214efa690f351de801bdead34ef99e4c1df9d
eduardoandd <eduardo.andrade.dev@gmail.com>
2025-01-26 01:02:34 -0300

    headers

--- my-app/src/app/(users)/profile/api/route.tsx
+++ my-app/src/app/(users)/profile/api/route.tsx
@@ -1,3 +1,16 @@
-export async function  GET() {
-    return new Response('profile')
+import { NextRequest } from "next/server"
+import { headers } from "next/headers"
+
+export async function  GET(request: NextRequest) {
+    const requestHeaders = new Headers(request.headers)
+    const headerList = headers()
+
+
+    console.log(requestHeaders.get('Authorization'));
+    console.log(headerList.get('Authorization'));
+    
+
+    return new Response('<h1>Profile Route</h1>',{ headers:{"Content-Type":"text/html"}})
+
+    
 }
