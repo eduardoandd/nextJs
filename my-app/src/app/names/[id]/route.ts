be43a3ea27fe93656dff4b5f7538c29151eeb879
eduardoandd <eduardo.andrade.dev@gmail.com>
2025-01-26 00:23:15 -0300

    filtro

--- my-app/src/app/names/[id]/route.ts
+++ my-app/src/app/names/[id]/route.ts
@@ -1,10 +1,12 @@
+import { NextUrlWithParsedQuery } from "next/dist/server/request-meta";
 import { data } from "../data";
+import { NextRequest } from "next/server";
 
-export async function GET(_request:Request,{params}: {params: {id:string}}) {
-    const name = data.find(name => name.id === parseInt(params.id))
-    return Response.json(name)
+// export async function GET(_request:Request,{params}: {params: {id:string}}) {
+//     const name = data.find(name => name.id === parseInt(params.id))
+//     return Response.json(name)
     
-}
+// }
 
 export async function PATCH(
     request:Request,
@@ -30,7 +32,14 @@ export async function DELETE(
     const deletedUser = data[index]
     data.splice(index,1)
     return Response.json(deletedUser)
+}
 
+export async function GET(request:NextRequest) {
+    const searchParams = request.nextUrl.searchParams
+    const query = searchParams.get('query')
 
+    const filteredNames = query ? data.filter(name => name.name.includes(query)): data
+
+    return Response.json(filteredNames)
 
 }
